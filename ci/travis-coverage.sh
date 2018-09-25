23527e4563ee520510e61e0ce96b7302f89f8e3e
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-09-25 17:45:02 +0200

    Debug gcov-6 path

--- ci/travis-coverage.sh
+++ ci/travis-coverage.sh
@@ -4,6 +4,8 @@ if [ "$BUILD_TYPE" == "Coverage" ]; then
   cd ${TRAVIS_BUILD_DIR}
   echo ${GCOV}
   echo $GCOV
+  gcov-6 --version
+  which gcov-6
   alias lcov="lcov --gcov-tool $(which ${GCOV})"
   lcov -d . -c -o coverage.info
   lcov -r coverage.info '/usr/*' 'test/*' 'libs/*' -o coverage.info
