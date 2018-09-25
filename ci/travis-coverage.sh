b3c6a27391c35cc51a8d2b4464fb47cee035245f
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-09-25 17:20:03 +0200

    Add shell script for coverage

---
+++ ci/travis-coverage.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$BUILD_TYPE" == "Coverage" ]; then
+  cd ${TRAVIS_BUILD_DIR}
+  alias lcov="lcov --gcov-tool $(which $(GCOV))"
+  lcov -d . -c -o coverage.info
+  lcov -r coverage.info '/usr/*' 'test/*' 'libs/*' -o coverage.info
+  lcov -l coverage.info
+  bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
+fi
