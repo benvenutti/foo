d63bb54f3c4eee209262525216a7fe2bcdcea5ab
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-09-25 17:52:12 +0200

    Test alias

--- ci/travis-coverage.sh
+++ ci/travis-coverage.sh
@@ -6,9 +6,9 @@ if [ "$BUILD_TYPE" == "Coverage" ]; then
   echo $GCOV
   gcov-6 --version
   which gcov-6
-  alias lcov="lcov --gcov-tool $(which ${GCOV})"
-  lcov -d . -c -o coverage.info
-  lcov -r coverage.info '/usr/*' 'test/*' 'libs/*' -o coverage.info
-  lcov -l coverage.info
+  alias lcov_="lcov --gcov-tool $(which ${GCOV})"
+  lcov_ -d . -c -o coverage.info
+  lcov_ -r coverage.info '/usr/*' 'test/*' 'libs/*' -o coverage.info
+  lcov_ -l coverage.info
   bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
 fi
