e8246dcfeb9c2af17f4c6b47bfb1dbf873f61aff
Diogo Andrei Benvenutti <diogo.a.benvenutti@gmail.com>
2018-09-25 18:05:50 +0200

    Remove alias

--- ci/travis-coverage.sh
+++ ci/travis-coverage.sh
@@ -2,13 +2,9 @@
 
 if [ "$BUILD_TYPE" == "Coverage" ]; then
   cd ${TRAVIS_BUILD_DIR}
-  echo ${GCOV}
-  echo $GCOV
-  gcov-6 --version
-  which gcov-6
-  alias lcov_="lcov --gcov-tool $(which ${GCOV})"
-  lcov_ -d . -c -o coverage.info
-  lcov_ -r coverage.info '/usr/*' 'test/*' 'libs/*' -o coverage.info
-  lcov_ -l coverage.info
+  gcov_tool="--gcov-tool $(which ${GCOV})"
+  lcov ${gcov_tool} -d . -c -o coverage.info
+  lcov ${gcov_tool} -r coverage.info '/usr/*' 'test/*' 'libs/*' -o coverage.info
+  lcov ${gcov_tool} -l coverage.info
   bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
 fi
